7ba4d57bdfcb565e63eb64deff1804a881b6777f
razziedazzel <68195773+razziedazzel@users.noreply.github.com>
2023-03-28 21:57:19 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,17 @@
 //The first line
+//All the Libraries I might need
+
+#include <iostream>
+#include <string>
+#include <exiv2/exiv2.hpp>
+#include <gphoto2/gphoto2.h>
+#include <wiringPi.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/rfcomm.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+#include <errno.h>
+#include <TFT_eSPI.h>
